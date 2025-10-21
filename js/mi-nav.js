308ecbe7b0bb832943e213ecb7100683901d44ce
GonzaloDan12 <146907133+GonzaloDan12@users.noreply.github.com>
2025-10-20 21:19:12 -0600

    Add files via upload

---
+++ js/mi-nav.js
@@ -0,0 +1,58 @@
+import {
+  ACTIVE_CLASS
+} from "../lib/js/constantes.js"
+
+export class MiNav
+  extends HTMLElement {
+  connectedCallback() {
+    this.classList.add("navTab")
+    this.innerHTML = /* html */
+      `<a href="index.html">
+     <span class="material-symbols-outlined">
+      camera
+     </span>
+     camara
+    </a>
+    <a href="geolocalizacion.html">
+     <span class="material-symbols-outlined">
+      hub
+     </span>
+     GPS
+    </a>
+    <a href="imagen.html">
+     <span class="material-symbols-outlined">
+      image
+     </span>
+     Imagen
+    </a>
+    <a href="video.html">
+     <span class="material-symbols-outlined">
+      TV
+     </span>
+     video
+    </a>
+    <a href="ayuda.html">
+     <span class="material-symbols-outlined">
+      help
+     </span>
+     Ayuda
+    </a>
+    `
+
+    const href = location.href
+    const index = this.querySelector(
+      "[href='index.html']")
+    const ayuda = this.querySelector(
+      "[href='ayuda.html']")
+    if (ayuda != null &&
+      href.endsWith("ayuda.html")) {
+      ayuda.classList.
+        add(ACTIVE_CLASS)
+    } else if (index != null) {
+      index.classList.
+        add(ACTIVE_CLASS)
+    }
+  }
+}
+customElements.define(
+  "mi-nav", MiNav)
